73fe0229e301ddcb633b1741bc7f7204726eb6e5
neetukumari4858 <neetu21@navgurukul.org>
2022-03-26 20:25:09 +0530

    Minor changes
    Minor changes

--- src/Components/Navigation/Navbar.js
+++ src/Components/Navigation/Navbar.js
@@ -32,27 +32,3 @@ const Navbar = () => {
 }
 export {Navbar};
 
-// <!-- <nav className='sum'>
-//     <div className="nav-section">
-//         <label className="logo">Royal</label> 
-//         <ul className="nav-home-shop-link">
-//             <li><Link className='nav-link icon-content' to ="/">  Home</Link></li>      
-//             <li><Link className='nav-link icon-content' to ="/ProductPage">  Shop</Link></li>
-//             <li><Link className='nav-link icon-content' to ="/ProductPage">  Men</Link></li>
-//             <li><Link className='nav-link icon-content' to ="/Product">  Woman</Link></li>
-//         </ul>
-//     </div>
-//     <div className="nav-section">
-//         <div className="outer-search-bar-div">
-
-//             <input type="text" className="search-bar" placeholder="   Search.."></input>
-//         </div>
-//         <ul className="nav-icons">
-//             <i className='fas fa-user-alt login-icon'><li>
-//                 <Link className="nav-link icon-content" to ="/LoginPage">Login</Link></li>
-//             </i>
-//             <i className="fa fa-heart cart-wishlist-icon"><li><Link className="nav-link icon-content" to ="/CartPage"> Cart</Link></li></i>
-//             <i className="fa fa-shopping-cart cart-wishlist-icon"><li><Link className="nav-link icon-content" to ="/WishlistPage"> Whishlist</Link></li></i>
-//         </ul>
-//     </div>
-// </nav>  -->
