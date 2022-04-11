10846d0c80538bb21476b26de6067bf0132b4f30
neetukumari4858 <90403664+neetukumari4858@users.noreply.github.com>
2022-04-11 10:12:34 +0530

    Added Provider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from "./App";
 import { makeServer } from "./server";
+import { ProductProvider } from "./Context/Context";
 import {BrowserRouter } from "react-router-dom";
 
 // Call make Server
@@ -11,7 +12,9 @@ makeServer();
 
 ReactDOM.render(
   <BrowserRouter>
+    <ProductProvider>
       <App/>
+   </ProductProvider>
 </BrowserRouter>,
   document.getElementById("root")
 );
