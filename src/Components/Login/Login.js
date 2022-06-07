43b7d820eb6573990723198c042ed37d8416ba6c
neetukumari4858 <90403664+neetukumari4858@users.noreply.github.com>
2022-06-07 11:18:02 +0530

    refector:changed  Auth UI Part

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "./Login.css"
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai'
+
+import './Login.css'
 const Login = () => {
+  const [inputType, setinputType] = useState('password')
+  const [loginUserData, setloginUserData] = useState({
+    email: '',
+    password: '',
+    checkPolicy: false,
+  })
+
   return (
-    <div className="outer-product-side-bar">
-        <div className="login-container">
-            <div className="login-content">
-                <h2 >Login</h2>
-                <label>Email address</label><br/>
-                <input className="user-input" placeholder="annu@neog.com"/><br/><br/>
-
-                <label>Password</label><br></br>
-                <input className="user-input" placeholder="..................."/><br/><br/>
-
-                <input type="checkbox"/> Remember me  &nbsp; &nbsp; &nbsp; <Link className='forgot-password' to ="/SignUpPage">Forgot Password ?</Link>
-                <button className="login-btn">Login</button>
-                <h4>Create New Account <i className='fas fa-angle-right'></i></h4>
+    <div className="outer-Login-container">
+      <div className="login-outer-container">
+        <form className="login-content-container">
+          <div className="login-content-container">
+            <h2 className="Login-heading-two">Login</h2>
+            <label className="lebel-text">Email address</label>
+            <input
+              className="user-input"
+              type="email"
+              placeholder="  demo@gmail.com "
+              value={loginUserData.email}
+              onChange={(event) =>
+                setloginUserData((prev) => ({
+                  ...prev,
+                  email: event.target.value,
+                }))
+              }
+              required
+            />
+            <label className="lebel-text">Password</label>
+            <input
+              type={inputType}
+              className="user-input password"
+              placeholder="  Enter Password..."
+              value={loginUserData.password}
+              onChange={(event) =>
+                setloginUserData((prev) => ({
+                  ...prev,
+                  password: event.target.value,
+                }))
+              }
+              required
+            />
+            <div
+              className="passwordIcon"
+              onClick={() =>
+                inputType === 'text'
+                  ? setinputType('password')
+                  : setinputType('text')
+              }
+            >
+              {inputType === 'text' ? (
+                <p>
+                  <AiFillEye />
+                </p>
+              ) : (
+                <p>
+                  <AiFillEyeInvisible />
+                </p>
+              )}
             </div>
-        </div>
-        
+            <div className="footerDiv">
+              <div className="login-grid">
+                <button type="button" className="videologin-btn bg-red">
+                  Login
+                </button>
+                <button
+                  type="button"
+                  className="videologin-btn  bg-white"
+                  onClick={() => {
+                    setloginUserData({
+                      ...loginUserData,
+                      email: 'demo@gmail.com',
+                      password: 'demo123',
+                      checkPolicy: true,
+                    })
+                  }}
+                >
+                  Guest Login
+                </button>
+              </div>
+
+              <h4 className="create-account">
+                <Link to="/SignUpPage" className="createAccount">
+                  Create New Account
+                </Link>
+              </h4>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
-export {Login}
-
+export { Login }
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,33 +1,126 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import "./SignUp.css";
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './SignUp.css'
+import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai'
+
 const SignUp = () => {
-    return (
-         <div className="outer-product-side-bar">
-            <div className="signup-container">
-                <div className="login-content">
-                    <h2>Sign up</h2>
-                    <label>First Name</label><br/>
-                    <input type= "text" className="user-input" placeholder="Enter your Name"/><br/>
+  const [inputType, setinputType] = useState('password')
 
-                    <label>Last Name</label><br></br>
-                    <input type= "text" className="user-input" placeholder="Enter your Last Name"/><br/>
+  const [newUser, setNewUser] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    firstName: '',
+    lastName: '',
+    checkPolicy: false,
+  })
 
-                    <label>Email address</label><br></br>
-                    <input type= "text" className="user-input" placeholder="annu@neog.com"/><br/>
+  return (
+    <div className="outer-Login-container">
+      <div className="signup-container">
+        <div className="login-content">
+          <h2 className="signUp-heading">Sign up</h2>
+          <label className="signUp-lebel">First Name</label>
+          <input
+            type="text"
+            className="sign-input"
+            placeholder="Enter your Name"
+            onChange={(event) =>
+              setNewUser((prev) => ({ ...prev, firstName: event.target.value }))
+            }
+          />
 
-                    <label>Password</label><br></br>
-                    <input type= "text" className="user-input" placeholder="..................."/><br/>
+          <label className="signUp-lebel">Last Name</label>
+          <input
+            type="text"
+            className="sign-input"
+            placeholder="Enter your Last Name"
+            onChange={(event) =>
+              setNewUser((prev) => ({ ...prev, lastName: event.target.value }))
+            }
+            required
+          />
 
-                    <label>Confirm Password</label><br></br>
-                    <input type= "text" className="user-input" placeholder="..................."/><br></br>
+          <label className="signUp-lebel">Email address</label>
+          <input
+            type="text"
+            className="sign-input"
+            placeholder="annu@neog.com"
+            onChange={(event) =>
+              setNewUser((prev) => ({ ...prev, email: event.target.value }))
+            }
+            required
+          />
 
-                    <input type="checkbox"/>I accept all Terms & Conditions
-                    <button className="login-btn">Sign up</button>
-                    <h4>Already have an account ? <Link to="/LoginPage">Login Here</Link></h4>
-                </div>
-            </div>
+          <label className="signUp-lebel">Password</label>
+          <input
+            type={inputType}
+            className="sign-input"
+            onChange={(event) =>
+              setNewUser((prev) => ({ ...prev, password: event.target.value }))
+            }
+            required
+          />
+          <div
+            className="signup_passwordIcon"
+            onClick={() =>
+              inputType === 'text'
+                ? setinputType('password')
+                : setinputType('text')
+            }
+          >
+            {inputType === 'text' ? (
+              <p>
+                <AiFillEye />
+              </p>
+            ) : (
+              <p>
+                <AiFillEyeInvisible />
+              </p>
+            )}
+          </div>
+
+          <label className="signUp-lebel confirm-Password">
+            Confirm Password
+          </label>
+          <input
+            type="text"
+            className="sign-input"
+            onChange={(event) =>
+              setNewUser((prev) => ({
+                ...prev,
+                confirmPassword: event.target.value,
+              }))
+            }
+            required
+          />
+          <br />
+          <div className="termsandConditions-div">
+            <input
+              type="checkbox"
+              id="termsAndCondition"
+              checked={newUser.checkPolicy}
+              onChange={() =>
+                setNewUser({ ...newUser, checkPolicy: !newUser.checkPolicy })
+              }
+            />
+            <label htmlFor="termsAndCondition" className="terms-and-condition">
+              I accept all Terms & Conditions
+            </label>
+          </div>
+
+          <div>
+            <button className="sign-btn bg-red">Sign up</button>
+          </div>
+          <h4 className="alreadyHaveAccount">
+            Already have an account?
+            <Link to="/LoginPage" className="login-here">
+              Login Here
+            </Link>
+          </h4>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
-export {SignUp}
+export { SignUp }
